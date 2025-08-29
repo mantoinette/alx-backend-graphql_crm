736c0b83810612ae8e4dbe03ae6365fb6bcf7b0d
mantoinette <antoni.kigali@gmail.com>
2025-08-29 11:37:20 +0200

    debuging

--- crm/settings.py
+++ crm/settings.py
@@ -77,7 +77,12 @@ CELERY_BEAT_SCHEDULE = {
 }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "crm_db",
+        "USER": "root",
+        "PASSWORD": "",  
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
+
